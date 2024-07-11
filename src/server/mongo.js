cde27dfbfa52454ee56c7c94c4c322e0f2c50d82
2ushar03 <143212538+2ushar03@users.noreply.github.com>
2024-07-11 18:51:01 +0530

    Update mongo.js

--- src/server/mongo.js
+++ src/server/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/FoodX")
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("MongoDB Connected");
     })
