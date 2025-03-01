6063e14167986aa6ccbaa2e321af81f696bfa5c1
Tushar Patil <143212538+2ushar03@users.noreply.github.com>
2025-03-01 16:08:58 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,111 +1,111 @@
-const data={
-    productItems:[
+const data = {
+  productItems: [
     {
-        id:"1",
-        title:"Big Spicy Paneer Wrap Combo",
-        info:"Enjoy Panner Wrap + Fries + Drink",
-        price:360.5,
-        image:"/src/resources/Spicypannerwrap.png"
+      id: "1",
+      title: "Big Spicy Paneer Wrap Combo",
+      info: "Enjoy Panner Wrap + Fries + Drink",
+      price: 360.5,
+      image: "/resources/Spicypannerwrap.png"  // Updated path
     },
     {
-        id:"13",
-        title:"Ice Cream",
-        info:"Yummy!...",
-        price:60,
-        image:"/src/resources/Icecream.png"
+      id: "13",
+      title: "Ice Cream",
+      info: "Yummy!...",
+      price: 60,
+      image: "/resources/Icecream.png"  // Updated path
     },
     {
-        id:"14",
-        title:"Paav Bhaji",
-        info:"Tasty!...",
-        price:89,
-        image:"/src/resources/paavbhaji.png"
+      id: "14",
+      title: "Paav Bhaji",
+      info: "Tasty!...",
+      price: 89,
+      image: "/resources/paavbhaji.png"  // Updated path
     },
     {
-        id:"2",
-        title:"Cake And Desert",
-        info:"Enjoy Cake For Celebrations Buy1 Get 1 free",
-        price:380,
-        image:"/src/resources/cakeanddesert.png"
+      id: "2",
+      title: "Cake And Desert",
+      info: "Enjoy Cake For Celebrations Buy1 Get 1 free",
+      price: 380,
+      image: "/resources/cakeanddesert.png"  // Updated path
     },
     {
-        id:"3",
-        title:"Dosa",
-        info:"Yummy!...",
-        price:80,
-        image:"/src/resources/dosa.png"
+      id: "3",
+      title: "Dosa",
+      info: "Yummy!...",
+      price: 80,
+      image: "/resources/dosa.png"  // Updated path
     },
     {
-        id:"4",
-        title:"Idli",
-        info:"Enjoy Idli",
-        price:30,
-        image:"/src/resources/idli.png"
+      id: "4",
+      title: "Idli",
+      info: "Enjoy Idli",
+      price: 30,
+      image: "/resources/idli.png"  // Updated path
     },
     {
-        id:"5",
-        title:"Juice",
-        info:"Enjoy Juice To Be Fit",
-        price:45,
-        image:"/src/resources/juice.png"
+      id: "5",
+      title: "Juice",
+      info: "Enjoy Juice To Be Fit",
+      price: 45,
+      image: "/resources/juice.png"  // Updated path
     },
     {
-        id:"6",
-        title:"Momos",
-        info:"Enjoy Momos",
-        price:70,
-        image:"/src/resources/momos.png"
+      id: "6",
+      title: "Momos",
+      info: "Enjoy Momos",
+      price: 70,
+      image: "/resources/momos.png"  // Updated path
     },
     {
-        id:"7",
-        title:"2 McAloo Tikki Burger + 2 Fries",
-        info:"Enjoy Tikki Burger with fries",
-        price:327.95,
-        image:"/src/resources/AlooTikiBurger.avif"
+      id: "7",
+      title: "2 McAloo Tikki Burger + 2 Fries",
+      info: "Enjoy Tikki Burger with fries",
+      price: 327.95,
+      image: "/resources/AlooTikiBurger.avif"  // Updated path
     },
     {
-        id:"8",
-        title:"Margreta",
-        info:"Enjoy Margreta Pizza ",
-        price:349,
-        image:"/src/resources/Margretta.avif"
+      id: "8",
+      title: "Margreta",
+      info: "Enjoy Margreta Pizza",
+      price: 349,
+      image: "/resources/Margretta.avif"  // Updated path
     },
     {
-        id:"9",
-        title:"Spicy Chicken Burger",
-        info:"Enjoy Chicken Burger + fries +Drink",
-        price:388.95,
-        image:"/src/resources/McSpicyChicken.avif"
+      id: "9",
+      title: "Spicy Chicken Burger",
+      info: "Enjoy Chicken Burger + fries +Drink",
+      price: 388.95,
+      image: "/resources/McSpicyChicken.avif"  // Updated path
     },
     {
-        id:"10",
-        title:"Peri Peri Pizza - Freshly Hand Tossed 7",
-        info:"Enjoy Peri Peri Pizza",
-        price:359,
-        image:"/src/resources/Periperi.avif"
+      id: "10",
+      title: "Peri Peri Pizza - Freshly Hand Tossed 7",
+      info: "Enjoy Peri Peri Pizza",
+      price: 359,
+      image: "/resources/Periperi.avif"  // Updated path
     },
     {
-        id:"11",
-        title:"New York Style Cheese",
-        info:"Enjoy the Classic New York Pizza! Topped with 100% whole milk Mozzarella",
-        price:199,
-        image:"/src/resources/Vegcorn.avif"
+      id: "11",
+      title: "New York Style Cheese",
+      info: "Enjoy the Classic New York Pizza! Topped with 100% whole milk Mozzarella",
+      price: 199,
+      image: "/resources/Vegcorn.avif"  // Updated path
     },
     {
-        id:"12",
-        title:"Veg Corn Pizza",
-        info:"Enjoy Veg Corn Pizza",
-        price:200,
-        image:"/src/resources/cheese.avif"
+      id: "12",
+      title: "Veg Corn Pizza",
+      info: "Enjoy Veg Corn Pizza",
+      price: 200,
+      image: "/resources/cheese.avif"  // Updated path
     },
     {
-        id:"15",
-        title:"Veg Roll",
-        info:"Yummy!...",
-        price:80,
-        image:"/src/resources/vegroll.png"
+      id: "15",
+      title: "Veg Roll",
+      info: "Yummy!...",
+      price: 80,
+      image: "/resources/vegroll.png"  // Updated path
     },
-]
+  ]
 };
 
 export default data;
